35a01b4c44a543e654d37ee9753ab4753dd06e49
wanghanchao <635085026@qq.com>
2022-01-20 14:17:39 +0800

    code change

--- example-i18n/src/main/java/com/example/spring/config/MyLocaleResolver.java
+++ example-i18n/src/main/java/com/example/spring/config/MyLocaleResolver.java
@@ -1,12 +1,15 @@
 package com.example.spring.config;
 
 
+import org.assertj.core.util.Lists;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 
 /**
  * 在链接上携带区域信息
@@ -14,12 +17,22 @@ import java.util.Locale;
 
 public class MyLocaleResolver implements LocaleResolver {
 
+    private static List<String> langType = Lists.newArrayList("en_US", "zh_CN", "zh_TW");
+
     @Override
     public Locale resolveLocale(HttpServletRequest request) {
-        String l = request.getParameter("l");
+        String acceptLanguage = request.getHeader("accept-language");
+        if (StringUtils.hasLength(acceptLanguage)) {
+            Optional<String> languageOpt = langType.stream().filter(acceptLanguage::contains).findFirst();
+            if (languageOpt.isPresent()) {
+                acceptLanguage = languageOpt.get();
+            }
+        } else {
+            acceptLanguage = request.getParameter("l");
+        }
         Locale locale = Locale.getDefault();
-        if (StringUtils.hasLength(l)) {
-            String[] split = l.split("_");
+        if (StringUtils.hasLength(acceptLanguage) && acceptLanguage.contains("_")) {
+            String[] split = acceptLanguage.split("_");
             locale = new Locale(split[0], split[1]);
         }
         return locale;
--- example-i18n/src/main/java/com/example/spring/controller/I18nController.java
+++ example-i18n/src/main/java/com/example/spring/controller/I18nController.java
@@ -3,7 +3,6 @@ package com.example.spring.controller;
 import com.example.spring.domain.entity.RspWrapper;
 import com.example.spring.utils.MsgHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,11 +14,11 @@ public class I18nController {
 
     @RequestMapping(value = "/change")
     @ResponseBody
-    public RspWrapper changeLocal(String l) {
-        System.out.println(LocaleContextHolder.getLocale().getLanguage());
+    public RspWrapper changeLocal() {
         RspWrapper res = new RspWrapper<>();
-        System.out.println(msgHelper.get("500"));
-        res.setData(msgHelper.get("500"));
+        res.setData(msgHelper.get("name"));
+        res.setCode(200);
+        res.setMsg("ok");
         return res;
     }
 }
