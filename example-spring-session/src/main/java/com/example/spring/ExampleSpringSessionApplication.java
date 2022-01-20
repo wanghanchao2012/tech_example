b913d08d1715798777f94eef8757091e85cc9f80
wanghanchao <635085026@qq.com>
2022-01-20 17:43:42 +0800

    code change

---
+++ example-spring-session/src/main/java/com/example/spring/ExampleSpringSessionApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableRedisHttpSession
+public class ExampleSpringSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringSessionApplication.class, args);
+    }
+
+}
---
+++ example-spring-session/src/main/java/com/example/spring/controller/SessionShareController.java
@@ -0,0 +1,37 @@
+package com.example.spring.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * SessionShareController <br>
+ * 〈session共享控制器〉
+ */
+@RestController
+@RequestMapping(value = "/session")
+public class SessionShareController {
+
+    @Value("${server.port}")
+    Integer port;
+
+
+    @GetMapping(value = "/set")
+    public String set(HttpServletRequest httpRequest) {
+
+        HttpSession session = httpRequest.getSession(true);
+
+        session.setAttribute("user", "123456");
+        return String.valueOf(port);
+    }
+
+    @GetMapping(value = "get")
+    public String get(HttpSession session) {
+        return "用户:" + session.getAttribute("user") + ",端口:" + port;
+    }
+}
