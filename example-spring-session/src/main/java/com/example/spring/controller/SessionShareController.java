60a1bcf9c05b4d42db6f7e024852d9b51fdb0a9f
wanghanchao <635085026@qq.com>
2022-06-06 10:54:28 +0800

    code change

--- example-spring-session/src/main/java/com/example/spring/controller/SessionShareController.java
+++ example-spring-session/src/main/java/com/example/spring/controller/SessionShareController.java
@@ -26,7 +26,7 @@ public class SessionShareController {
 
         HttpSession session = httpRequest.getSession(true);
         session.setAttribute("user", "123456");
-        System.out.println("--hello world--");
+        System.out.println("--hello world2022222222--");
         return String.valueOf(port);
     }
 
