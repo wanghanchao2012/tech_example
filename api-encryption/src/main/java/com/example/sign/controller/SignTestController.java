e13cf2405e105f604ae991e8a14a0996fd5db78c
wanghanchao <635085026@qq.com>
2022-06-21 14:54:36 +0800

    add code

---
+++ api-encryption/src/main/java/com/example/sign/controller/SignTestController.java
@@ -0,0 +1,28 @@
+package com.example.sign.controller;
+
+import com.example.sign.config.response.ResponseResult;
+import com.example.sign.config.sign.Signature;
+import com.example.sign.entity.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pdai
+ */
+@RestController
+@RequestMapping("user")
+public class SignTestController {
+
+    @Signature
+    @PostMapping("test/{id}")
+    public ResponseResult<String> myController(@PathVariable String id
+            , @RequestParam String client
+            , @RequestBody User user) {
+        return ResponseResult.success(String.join(",", id, client, user.toString()));
+    }
+
+}
