9613cd158e71d41672339d202153421285c84efd
wanghanchao2012 <35331347+wanghanchao2012@users.noreply.github.com>
2022-11-02 09:28:55 +0800

    code change

--- 34-example-apache-common/src/main/java/com/example/dbutil/dao/Test.java
+++ 34-example-apache-common/src/main/java/com/example/dbutil/dao/Test.java
@@ -6,6 +6,6 @@ import com.example.dbutil.dao.impl.EmpDaoImpl;
 public class Test {
     public static void main(String[] args) {
         Employee zhang = new EmpDaoImpl().selectEmpByLoginAccount("zhang");
-        System.out.println(zhang.getFirst() + zhang.getLast());
+        System.out.println("姓名：" + zhang.getFirst() + zhang.getLast() + ",芳龄：" + zhang.getAge());
     }
 }
