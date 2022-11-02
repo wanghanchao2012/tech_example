ba131cb7ac687bf53e3d010a22072a4325572449
wanghanchao2012 <35331347+wanghanchao2012@users.noreply.github.com>
2022-11-02 09:33:20 +0800

    add document

--- 34-example-apache-common/src/main/java/com/example/dbutil/dao/BaseDao.java
+++ 34-example-apache-common/src/main/java/com/example/dbutil/dao/BaseDao.java
@@ -28,10 +28,9 @@ public class BaseDao<T> {
      * @return 查询结果
      */
     public List<T> getBeanList(String sql, Class<T> entityClass, Object... parameters) {
+        // 获取数据库连接
+        Connection connection = JDBCUtils.getConnection();
         try {
-            // 获取数据库连接
-            Connection connection = JDBCUtils.getConnection();
-
             return runner.query(connection, sql, new BeanListHandler<>(entityClass), parameters);
 
         } catch (SQLException e) {
@@ -39,8 +38,9 @@ public class BaseDao<T> {
 
             // 如果真的抛出异常，则将编译时异常封装为运行时异常抛出
             new RuntimeException(e);
+        } finally {
+            JDBCUtils.releaseConnection(connection);
         }
-
         return null;
     }
 
@@ -53,11 +53,9 @@ public class BaseDao<T> {
      * @return 查询到的实体类对象
      */
     public T getSingleBean(String sql, Class<T> entityClass, Object... parameters) {
-
+        // 获取数据库连接
+        Connection connection = JDBCUtils.getConnection();
         try {
-            // 获取数据库连接
-            Connection connection = JDBCUtils.getConnection();
-
             return runner.query(connection, sql, new BeanHandler<>(entityClass), parameters);
 
         } catch (SQLException e) {
@@ -65,8 +63,9 @@ public class BaseDao<T> {
 
             // 如果真的抛出异常，则将编译时异常封装为运行时异常抛出
             new RuntimeException(e);
+        } finally {
+            JDBCUtils.releaseConnection(connection);
         }
-
         return null;
     }
 
@@ -78,10 +77,8 @@ public class BaseDao<T> {
      * @return 受影响的行数
      */
     public int update(String sql, Object... parameters) {
-
+        Connection connection = JDBCUtils.getConnection();
         try {
-            Connection connection = JDBCUtils.getConnection();
-
             int affectedRowNumbers = runner.update(connection, sql, parameters);
 
             return affectedRowNumbers;
@@ -92,8 +89,9 @@ public class BaseDao<T> {
             new RuntimeException(e);
 
             return 0;
+        } finally {
+            JDBCUtils.releaseConnection(connection);
         }
-
     }
 
 }
--- 34-example-apache-common/src/main/java/com/example/dbutil/dao/impl/EmpDaoImpl.java
+++ 34-example-apache-common/src/main/java/com/example/dbutil/dao/impl/EmpDaoImpl.java
@@ -1,6 +1,6 @@
 package com.example.dbutil.dao.impl;
 
-import com.example.dbutil.BaseDao;
+import com.example.dbutil.dao.BaseDao;
 import com.example.dbutil.Employee;
 import com.example.dbutil.dao.api.EmpDao;
 
